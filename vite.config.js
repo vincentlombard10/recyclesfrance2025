bdc4d9e1dbb84d45c35c59d46d8e7b37148d244d
Vincent Lombard <vincentlombard@MacBook-Pro-de-Vincent.local>
2024-12-11 13:30:33 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 // import vue2 from '@vitejs/plugin-vue2';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
