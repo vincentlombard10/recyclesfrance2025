d16eada0680f7c6bf60aa760150aba239e5a9478
vincentlombard10 <vincent.lombard@re-cycles-france.fr>
2025-04-29 10:38:20 +0200

    commit

---
+++ config/statamic/seo-pro.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+
+    'site_defaults' => [
+        'path' => base_path('content/seo.yaml'),
+    ],
+
+    'assets' => [
+        'container' => null,
+        'twitter_preset' => [
+            'w' => 1200,
+            'h' => 600,
+        ],
+        'open_graph_preset' => [
+            'w' => 1146,
+            'h' => 600,
+        ],
+    ],
+
+    'sitemap' => [
+        'enabled' => true,
+        'url' => 'sitemap.xml',
+        'expire' => 60,
+        'pagination' => [
+            'enabled' => false,
+            'url' => 'sitemap_{page}.xml',
+            'limit' => 100,
+        ],
+    ],
+
+    'humans' => [
+        'enabled' => true,
+        'url' => 'humans.txt',
+    ],
+
+    'pagination' => [
+        'enabled_in_canonical_url' => true,
+        'enabled_on_first_page' => false,
+    ],
+
+    'twitter' => [
+        'card' => 'summary_large_image',
+    ],
+
+    'alternate_locales' => [
+        'enabled' => true,
+        'excluded_sites' => [],
+    ],
+
+    'reports' => [
+        'keep_recent' => 'all',
+        'queue_chunk_size' => 1000,
+    ],
+
+];
